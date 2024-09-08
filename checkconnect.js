705057d0b355782aba2169d74fe0b35146bd63b8
SoleneMarie <solene.plassart@gmail.com>
2024-09-08 21:23:13 +0200

    new try

--- checkconnect.js
+++ checkconnect.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 
+console.log(process.env.API_KEY.slice(0, 2) + process.env.API_KEY.slice(-2));
 try {
   axios.get(
     `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
